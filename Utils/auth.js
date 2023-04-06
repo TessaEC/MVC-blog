1e733e7f1b93d8b73045a949b6da860c252374dd
TessaEC <118077000+TessaEC@users.noreply.github.com>
2023-04-06 12:34:46 -0500

    final commit

--- Utils/auth.js
+++ Utils/auth.js
@@ -1,6 +1,6 @@
 const withAuth = (req, res, next) => { // check to see if my current session is logged in
     if (!req.session.logged_in) {
-      res.redirect('/login');
+      res.redirect('/');
     } else {
       next();
     }
